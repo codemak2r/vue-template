e027051b16145cb3e90cbc2d09f4c10be955ba55
zhongwei.wen <jackson@jacksons-MacBook-Pro.local>
2021-11-03 23:13:44 +0800

    setup element plus

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+
+createApp(App)
+    .use(ElementPlus, {locale})
+    .use(store)
+    .use(router)
+    .mount('#app')
