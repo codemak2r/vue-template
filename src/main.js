4f997d9a8da168973d47386d19ae04f7e0d1f264
zhongwei.wen <jackson@jacksons-MacBook-Pro.local>
2021-11-03 23:21:54 +0800

    setup bulma

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import locale from 'element-plus/lib/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
+import 'bulma/css/bulma.min.css'
+
 
 
 createApp(App)
